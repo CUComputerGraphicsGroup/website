e29f20a3bf9e6f452badd9acbe323635a5172367
Lesley <lesleylai@lesleylai.info>
2018-10-11 17:00:46 -0600

    Add past talks

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -11,6 +11,11 @@ const Activities = () => (
       <Container>
         <h1>Activities</h1>
         <h2>Talks</h2>
+        <h4>October 9, 2018: Josh talks on sprite sheets</h4>
+        <a href="https://git.io/fxZuH">Slides and codes</a>
+
+        <h4>October 2, 2018: Lesley Lai talks about Ray Tracing</h4>
+        <a href="http://lesleylai.info/slides/ray-tracing">slides</a>
       </Container>
     </Page>
   </IndexLayout>
