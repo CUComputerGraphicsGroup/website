dc1e2e956f016ff7f5c041a3fafe8ff98cd85eea
Lesley <lesleylai@lesleylai.info>
2018-10-11 16:57:20 -0600

    Style changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,19 +9,20 @@ import Container from './Container'
 const StyledHeader = styled.header`
   height: ${heights.header}px;
   padding: 0 ${dimensions.containerPadding}rem;
-  background-color: ${colors.brand};
   color: ${transparentize(0.5, colors.white)};
+  box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);
 `
 
 const HeaderInner = styled(Container)`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
   align-items: center;
   height: 100%;
 `
 
 const HomepageLink = styled(Link)`
-  color: ${colors.white};
+  color: ${colors.black};
   font-size: 1.5rem;
   font-weight: 600;
 
@@ -31,11 +32,21 @@ const HomepageLink = styled(Link)`
   }
 `
 
+const Menu = styled.div`
+  float: right;
+`
+
 const MenuLink = styled(Link)`
   color: ${colors.white};
-  font-size: 1rem;
+  font-size: 0.8em;
   font-weight: 600;
-  margin: 0 10px;
+
+  padding: 1em 1.5em;
+  letter-spacing: 1px;
+  text-decoration: none;
+  text-transform: uppercase;
+  font-size: 0.8em;
+  color: #2B3F48;
 
   &:hover,
   &:focus {
@@ -53,8 +64,13 @@ const Header: React.SFC<HeaderProps> = ({ title }) => (
     <HeaderInner>
       <HomepageLink to="/">{title}</HomepageLink>
 
-      <MenuLink to="/leadership">Leadership</MenuLink>
-      <MenuLink to="/activities">Activities</MenuLink>
+      <Menu>
+        <MenuLink to="/">Homepage</MenuLink>
+        <MenuLink to="/leadership">Leadership</MenuLink>
+        <MenuLink to="/activities">Activities</MenuLink>
+      </Menu>
+
+
     </HeaderInner>
   </StyledHeader>
 )
