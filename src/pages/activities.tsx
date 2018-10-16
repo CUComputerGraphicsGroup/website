8d2418d4e4ff000d7a374d59ab8a6c759c37044c
Lesley <lesleylai@lesleylai.info>
2018-10-16 08:50:11 -0600

    Add content about Graphics Hack Night

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -11,11 +11,16 @@ const Activities = () => (
       <Container>
         <h1>Activities</h1>
         <h2>Talks</h2>
-        <h4>October 9, 2018: Josh talks on sprite sheets</h4>
+        <h3>October 9, 2018: Josh talks on sprite sheets</h3>
         <a href="https://git.io/fxZuH">Slides and codes</a>
 
-        <h4>October 2, 2018: Lesley Lai talks about Ray Tracing</h4>
+        <h3>October 2, 2018: Lesley Lai talks about Ray Tracing</h3>
         <a href="http://lesleylai.info/slides/ray-tracing">slides</a>
+
+        <h2>Graphics Hack Night</h2>
+        <h3>Whitted Ray Tracer</h3>
+        Repository: <a href="https://github.com/CUComputerGraphicsGroup/Graphics-Hack-Night">https://github.com/CUComputerGraphicsGroup/Graphics-Hack-Night</a>
+
       </Container>
     </Page>
   </IndexLayout>
