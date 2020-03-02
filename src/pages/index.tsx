e2109fa6e00ed9bf3107f87d1c5e8a874aa49bc1
Lesley Lai <lesley@lesleylai.info>
2020-03-01 19:08:38 -0700

    Change contents

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,20 +9,9 @@ const IndexPage = () => (
   <IndexLayout>
     <Page>
       <Container>
-        <h1>Welcome to the official website for the CU Computer Graphics Group</h1>
-        <p>
-          The CU Computer Graphics Group is a student orgnization formed and maintained by students of <a href="https://www.colorado.edu/">University of Colorado</a>.
-        </p>
-
-        <p>
-          Our group was founded at Sebtember, 2018; Its predecessor is a club called "CU Computer Science Discussion Group".
-        </p>
-        <h2>Contact</h2>
-        <ul>
-          <li>Join the group at <a href="https://orgsync.com/156978/chapter">University portal</a> </li>
-          <li>Please also join the <a href="https://join.slack.com/t/cucomputergraphics/shared_invite/enQtMzgyMzgzNDY5ODg5LTAzMWNkZDY1MDY2YWE5MzY4OTkyNWI5ZmNkMmQ4ZTUzZWNmN2JjN2UxOGZiM2Y2YWI5YmE0OTBmNTAyY2NjNDU">slack channel</a> to chat</li>
-          <li>Email: graphics@colorado.edu</li>
-        </ul>
+        <h1>
+          As of February 2020, this club is merged with the CU Game Dev club. Please visit our new website at <a href="https://cugdc.github.io/">cugdc.github.io/</a>
+        </h1>
       </Container>
     </Page>
   </IndexLayout>
